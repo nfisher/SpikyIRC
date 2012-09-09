2562f9799f58e565b93ebb4f28e0a3d3f9d883cb
Nathan Fisher <nfisher@junctionbox.ca>
2012-09-09 14:34:50 +0100

    @nfisher: Refactor users manifest.

--- puppet/modules/users/manifests/irc.pp
+++ puppet/modules/users/manifests/irc.pp
@@ -1,32 +1,41 @@
-define users::irc($key, $ensure = 'present', $user = $title, $type = 'rsa') {
-  user { "$user":
+define users::irc($key, $fullname, $ensure = 'present', $username = $title, $type = 'rsa') {
+  if 'present' == $ensure {
+    $dir_ensure  = 'directory'
+    $file_ensure = 'file'
+  } else {
+    $dir_ensure  = 'absent'
+    $file_ensure = 'absent'
+  }
+
+  user { "$username":
     ensure     => $ensure,
+    comment    => $fullname,
     managehome => true,
     shell      => '/usr/bin/irssi',
     require    => Class['irssi'],
   }
 
-  file { "${user}_irssi_dir":
-    ensure  => directory,
-    path    => "/home/${user}/.irssi",
+  file { "${username}_irssi_dir":
+    ensure  => $dir_ensure,
+    path    => "/home/${username}/.irssi",
     mode    => '0755',
-    owner   => $user,
-    group   => $user,
-    require => User[$user],
+    owner   => $username,
+    group   => $username,
+    require => User[$username],
   }
   
-  file { "${user}_irssi_config":
-    ensure  => file,
-    path    => "/home/${user}/.irssi/config",
+  file { "${username}_irssi_config":
+    ensure  => $file_ensure,
+    path    => "/home/${username}/.irssi/config",
     content => template('users/irssi_config'),
-    require => File["${user}_irssi_dir"],
+    require => File["${username}_irssi_dir"],
   }
 
-  ssh_authorized_key { "${user}_key":
+  ssh_authorized_key { "${username}_key":
     ensure  => $ensure,
     key     => $key,
     type    => $type,
-    user    => $user,
-    require => User[$user],
+    user    => $username,
+    require => User[$username],
   }
 }
